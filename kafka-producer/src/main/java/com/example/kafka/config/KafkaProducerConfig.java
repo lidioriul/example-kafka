dbe38bca915974e7714fa632811f99767fcf4dce
Lidio Riul <lidioriul@gmail.com>
2021-03-18 23:00:27 -0300

    Commit incluind profiles no pom para exemplo

--- kafka-producer/src/main/java/com/example/kafka/config/KafkaProducerConfig.java
+++ kafka-producer/src/main/java/com/example/kafka/config/KafkaProducerConfig.java
@@ -18,9 +18,20 @@ public class KafkaProducerConfig {
     @Value("${kafka.bootstrapAddress}")
     private String bootstrapAddress;
 
+    @Value("${example.profile}")
+    private String exampleProperties;
+    @Value("${examplo.spring.datasource.username}")
+    private String dbUsername;
+    @Value("${examplo.spring.datasource.password}")
+    private String dbPassword;
+
     @Bean
     public ProducerFactory<String, String> producerFactory() {
-        System.out.println("[KAFKA-PRODUCER] Properties do kafka: " + bootstrapAddress);
+        System.out.println("[EXAMPLE-PROFILE] Exemplo de profile: " + exampleProperties);
+        System.out.println("[EXAMPLE-PROFILE] Exemplo de profile - USERNAME banco: " + dbUsername);
+        System.out.println("[EXAMPLE-PROFILE] Exemplo de profile - PASSWORD banco: " + dbPassword);
+
+        //System.out.println("[KAFKA-PRODUCER] Properties do kafka: " + bootstrapAddress);
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
